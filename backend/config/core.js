3119c2d7bb11366a1ad89853dcd313b1cdec83b5
michaelperryjnr <michaelperryt97@gmail.com>
2025-04-03 14:03:36 +0000

    Created server cores

---
+++ backend/config/core.js
@@ -0,0 +1,11 @@
+const STATUS_CODES = {
+    BAD_REQUEST: 400,
+    INTERNAL_SERVER_ERROR: 500,
+    UNAUTHORIZED: 403,
+    NOT_FOUND: 404,
+    CREATED: 201,
+    OK: 200
+}
+
+
+module.exports = {STATUS_CODES}
