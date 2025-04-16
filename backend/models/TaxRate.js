962ab57f67cf26f93030c1da94ea79467bf2abff
michaelperryjnr <michaelperryt97@gmail.com>
2025-04-16 17:00:50 +0000

    Created tax rate model:

---
+++ backend/models/TaxRate.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const taxRateSchema = new mongoose.Schema({
+  name: { type: String, required: true, trim: true },
+  rate: { type: Number, required: true, min: 0 },
+  appliesTo: { 
+    type: String, 
+    enum: ["Purchase", "Sale", "Both"], 
+    required: true 
+  }
+}, { timestamps: true });
+
+const TaxRate = mongoose.model("TaxRate", taxRateSchema);
+
+module.exports = TaxRate;
