31700e78c9ae40be069e3aa8fc16bf7199f9e64b
michaelperryjnr <michaelperryt97@gmail.com>
2025-04-17 10:26:31 +0000

    Update server entry

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,27 @@
 const { connectDB, disconnectDB } = require("./config/db");
 const { startServer, stopServer } = require("./server");
+const Logger = require("./utils/logger")
 
 async function initializeApp() {
   try {
+    Logger("Initializing application...", null, "api-core", "info");
     await connectDB();
 
     const server = startServer();
+    Logger("Server started successfully and DB connected", null, "api-core", "info");
 
     // gracefully shutdown and cleanup
     process.on("SIGINT", async () => {
+      Logger("Received SIGINT. Shutting down server and disconnecting DB...", null, "api-core", "info");
       await disconnectDB();
+      Logger("Server stopped and DB disconnected", null, "info");
       stopServer(server);
 
       console.log("Server stopped and DB disconnected");
       process.exit(0);
     });
   } catch (error) {
+    Logger("Error initializing application",null, "api-core","error", error);
     console.error("Error starting server", error);
     process.exit(1);
   }
